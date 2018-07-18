526f084872f8a29257765b3b6acaee1e04ae324b
yushaoyi <>
2018-07-18 23:02:33 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles/common.scss'
+import { Route, Switch, BrowserRouter as Router, Redirect } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <h1 className="App-title">Welcome to </h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Router>
+           <Switch>
+               <Route path='/appMain' component={}/>
+           </Switch>
+       </Router>
       </div>
     );
   }
---
+++ src/routes/appMain/appMain.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Route, Redirect, Link } from 'react-router-dom';
+
+class AppMain extends Component {
+    render () {
+        const { match } = this.props
+        return (
+            <div className="main">
+
+            </div>
+        )
+    }
+}
